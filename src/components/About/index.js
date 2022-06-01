1866fb1ba90f76b397e9d138a5f0edabdac696f9
robbiemurphy <rmurphy987@gmail.com>
2022-06-01 04:41:02 -0500

    main Header, Navigation, Project and Footer folders added to src, components

---
+++ src/components/About/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section>
+      <h1 id="about">Robbie Murphy / Pro Portfolio</h1>
+    </section>
+  );
+}
+
+export default About;
